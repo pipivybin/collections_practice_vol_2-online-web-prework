b7d9b5f922bd199ac3c1579ceae216036721750f
Ming Duan <11203duan@gmail.com>
2019-02-17 18:44:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
-# your code goes here
+def begins_with_r(a)
+  if a.all?  { |x|
+    x.start_with?("r")
+  }
+   true
+else  false
+end
+end
+
+def contain_a(a)
+  i = 0
+  arr = []
+  while i < a.length
+    if a[i].include?("a")
+      arr << a[i]
+    end
+      i += 1
+  end
+    arr
+end
+
+def first_wa(a)
+  a.find {
+|x| x.to_s.start_with?("wa")
+}
+end
+
+def remove_non_strings(a)
+      a.delete_if {|x| x.class != String}
+end
+
+def count_elements(a)
+  count
+  a.each do
+    |has| has.each do
+      |name, value|
+    end
+  end
+end
+
+def merge_data(a, b)
+  a.concat(b)
+end
+
+def find_cool(a)
+
+end
+
+def organize_schools(a)
+  a.sort
+end
