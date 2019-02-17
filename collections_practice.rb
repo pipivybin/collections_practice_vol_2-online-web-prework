723ea7fa12f777b4698c56868a8190d042001a9a
Ming Duan <11203duan@gmail.com>
2019-02-17 19:46:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,8 +45,9 @@ def merge_data(a, b)
 end
 
 def find_cool(a)
+binding.pry
   a.each do
-    binding.pry
+    
     |hash| hash.each do
       |subhash| subhash.each do
         |key, value| if value == "cool"
